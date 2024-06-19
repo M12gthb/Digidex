0fb0172f4584473f9f518cff393d1bbecaa8f9a4
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-06-19 11:07:32 -0300

    feat: not-found page

---
+++ digidex/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function NotFound() {
+  return (
+    <div className="flex items-center flex-col gap-4">
+      <Link href="/">
+        <Image src="/greymon.png" width={300} height={300} alt="Aplocalymon" />
+      </Link>
+      <h3 className="text-5xl text-gray-300 italic">404 Not found</h3>
+    </div>
+  );
+}
