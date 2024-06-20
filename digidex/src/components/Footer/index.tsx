03ec754f3bb4422877e184d0ec21359db2651ad1
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-06-20 10:34:54 -0300

    feat: add new ChadCN component Footer

---
+++ digidex/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="-full bg-zinc-900 min-h-[15vh] flex items-center justify-between px-[5%]"></footer>
+  );
+}
+
+export default Footer;
