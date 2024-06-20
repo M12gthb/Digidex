b7b2bd613744fe8545904de9f4c005e1594a1e74
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-06-20 10:38:34 -0300

    feat: main page

--- digidex/src/app/page.tsx
+++ digidex/src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { api } from "@/services/api";
 
 export default function Home() {
-  return (
-    <main>
-      <h1></h1>
-    </main>
-  );
+  const [digimos, setDigimons] = useState([]);
+  console.log(digimos);
+
+  const getDigimons = async () => {
+    const response = await api.get("/digimon?pageSize=1460");
+    setDigimons(response.data.content);
+  };
+
+  useEffect(() => {
+    getDigimons();
+  }, []);
+
+  return <main className=" bg-zinc-800 min-h-[70vh]"></main>;
 }
