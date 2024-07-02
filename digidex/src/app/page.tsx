45b707aea7e2dbbeb69d646b05202b9717c489d5
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-07-02 20:53:58 -0300

    feat: render page cards and pagination

--- digidex/src/app/page.tsx
+++ digidex/src/app/page.tsx
@@ -1,20 +1,78 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import { api } from "@/services/api";
+import { Cards } from "@/components/Cards";
+import { IDigimon, IDigimonInfos } from "@/interfaces/Digimon";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const [digimos, setDigimons] = useState([]);
-  console.log(digimos);
+  const [digimons, setDigimons] = useState<IDigimon[]>([]);
+  const [digimonsInfos, setDigimonsInfos] = useState<IDigimonInfos[]>([]);
+  const [displayedDigimons, setDisplayedDigimons] = useState<IDigimonInfos[]>(
+    []
+  );
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const DIGIMONS_PER_PAGE = 12;
 
   const getDigimons = async () => {
-    const response = await api.get("/digimon?pageSize=1460");
-    setDigimons(response.data.content);
+    try {
+      const response = await api.get("/digimon?pageSize=1460");
+      setDigimons(response.data.content);
+      console.log(digimons);
+      const infosPromises = response.data.content.map(
+        async (element: IDigimon) => {
+          const responseInfos = await api.get(element.href);
+          return responseInfos.data;
+        }
+      );
+
+      const infos = await Promise.all(infosPromises);
+      const favoriteDigimons = infos.map((info) => {
+        const isFavorite = localStorage.getItem(`favoriteDigimon-${info.id}`);
+        return { ...info, isFavorite: !!isFavorite };
+      });
+
+      setDigimonsInfos(favoriteDigimons);
+      setDisplayedDigimons(favoriteDigimons.slice(0, DIGIMONS_PER_PAGE));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const loadMoreDigimons = () => {
+    const nextPage = currentPage + 1;
+    const newDigimons = digimonsInfos.slice(
+      nextPage * DIGIMONS_PER_PAGE,
+      (nextPage + 1) * DIGIMONS_PER_PAGE
+    );
+    setDisplayedDigimons([...displayedDigimons, ...newDigimons]);
+    setCurrentPage(nextPage);
   };
 
   useEffect(() => {
     getDigimons();
   }, []);
 
-  return <main className=" bg-zinc-800 min-h-[70vh]"></main>;
+  return (
+    <main className="bg-zinc-800 min-h-[65vh] mt-[5vh] p-4">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mb-[10px]">
+        {displayedDigimons.map((digimon) => (
+          <li key={digimon.id} className="flex justify-center">
+            <Cards digimon={digimon} />
+          </li>
+        ))}
+      </ul>
+      {displayedDigimons.length < digimonsInfos.length && (
+        <div className="w-[100%] flex items-center justify-center">
+          <Button
+            onClick={loadMoreDigimons}
+            className="mt-4 p-2 bg-blue-500 text-white rounded"
+          >
+            Carregar mais Digimons
+          </Button>
+        </div>
+      )}
+    </main>
+  );
 }
