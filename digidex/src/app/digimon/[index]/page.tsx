16d9232362e8150fde49e4e5ad2f77c9982dbc51
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-07-02 20:51:24 -0300

    style: min heigth to digimon page

--- digidex/src/app/digimon/[index]/page.tsx
+++ digidex/src/app/digimon/[index]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function digimon({ params }: { params: { index: string } }) {
   return (
-    <div>
+    <div className="min-h-[70vh]">
       <h1>Digimon</h1>
       <h2>{params.index}</h2>
     </div>
