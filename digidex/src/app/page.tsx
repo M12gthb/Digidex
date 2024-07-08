786bd3c09a0a8283db34a7c39e2f001396722f88
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-07-08 09:52:39 -0300

    feat: add filter without functionality

--- digidex/src/app/page.tsx
+++ digidex/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDigimonContext } from "@/context/DigimonContext";
-import { Button } from "@/components/ui/button";
+import CustomInputHome from "@/components/CustomInputPage";
 import Cards from "@/components/Cards";
 import {
   Select,
@@ -10,9 +10,20 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import CustomInputHome from "@/components/CustomInputPage";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Button } from "@/components/ui/button";
+import {
+  selectLevel,
+  selectTypes,
+  selectAttribute,
+  selectField,
+  selectDate,
+} from "./data/selectItens";
+import { Popover, PopoverContent } from "@/components/ui/popover";
+import { PopoverTrigger } from "@/components/ui/popover";
+import { Label } from "@radix-ui/react-label";
 
-function Home() {
+const Home = () => {
   const {
     displayedDigimons,
     digimonsInfos,
@@ -25,18 +36,19 @@ function Home() {
     applyFiltersAndSort,
   } = useDigimonContext();
 
-  const handleInputSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const term = e.target.value;
-    setSearchTerm(term);
-    if (!term) {
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    setSearchTerm(searchInput);
+    if (!searchInput) {
       applyFiltersAndSort(digimonsInfos);
     } else {
-      const find = digimonsInfos.filter((digimon) =>
-        digimon.name.toLowerCase().includes(term.toLowerCase())
+      const filteredDigimons = digimonsInfos.filter((digimon) =>
+        digimon.name.toLowerCase().includes(searchInput.toLowerCase())
       );
-      applyFiltersAndSort(find);
+      applyFiltersAndSort(filteredDigimons);
     }
-  };
+  }, [searchInput, setSearchTerm, digimonsInfos, applyFiltersAndSort]);
 
   const handleSortOrder = (order: string) => {
     setCurrentSortOrder(order);
@@ -57,75 +69,27 @@ function Home() {
   };
 
   return (
-    <main className="bg-zinc-800 min-h-[65vh] mt-[5vh] p-4">
-      <section className="border-none mb-10 flex flex-col md:flex-row w-full gap-2.5 items-center justify-center">
-        <CustomInputHome onChange={handleInputSearch} value={searchTerm} />
+    <main className="bg-zinc-800 min-h-[70vh] p-4">
+      <section className="border-none mb-5 flex flex-col md:flex-row w-full gap-2.5 items-center justify-center">
+        <CustomInputHome
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchTerm}
+        />
         <div className="flex flex-col md:flex-row gap-2.5">
           <Select onValueChange={handleLevelFilter}>
             <SelectTrigger className="w-[180px] h-9 bg-black text-cyan-50 border-none">
-              <SelectValue placeholder="Nível" />
+              <SelectValue placeholder="Filtro" />
             </SelectTrigger>
             <SelectContent className="bg-black text-cyan-50 border-none cursor-pointer">
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="All"
-              >
-                All
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Baby I"
-              >
-                Baby I
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Baby II"
-              >
-                Baby II
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Child"
-              >
-                Child
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Adult"
-              >
-                Adult
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Perfect"
-              >
-                Perfect
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Ultimate"
-              >
-                Ultimate
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Armor"
-              >
-                Armor
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Unknown"
-              >
-                Unknown
-              </SelectItem>
-              <SelectItem
-                className="bg-black text-cyan-50 border-none cursor-pointer"
-                value="Hybrid"
-              >
-                Hybrid
-              </SelectItem>
+              {selectLevel.map((item) => (
+                <SelectItem
+                  key={item.value}
+                  className="bg-black text-cyan-50 border-none cursor-pointer"
+                  value={item.value}
+                >
+                  {item.label}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
           <Select onValueChange={handleSortOrder}>
@@ -145,10 +109,156 @@ function Home() {
               >
                 z to a
               </SelectItem>
+              <SelectItem
+                className="bg-black text-cyan-50 border-none cursor-pointer"
+                value="desId"
+              >
+                descendente
+              </SelectItem>
+              <SelectItem
+                className="bg-black text-cyan-50 border-none cursor-pointer"
+                value="ascId"
+              >
+                ascendente
+              </SelectItem>
             </SelectContent>
           </Select>
         </div>
       </section>
+      <div className="h-15 flex items-center w-full mb-7 justify-center">
+        <Popover>
+          <PopoverTrigger className="w-[250px] h-10  bg-blue-500 text-white rounded">
+            Filtro personalizado
+          </PopoverTrigger>
+          <PopoverContent className="bg-black">
+            <form className="w-full h-full flex flex-col gap-2" typeof="submit">
+              <div className="flex flex-col gap-2">
+                <h2 className="text-white">X-Antibody</h2>
+                <RadioGroup defaultValue="x-false" className="flex">
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem
+                      value="x-true"
+                      id="option-one"
+                      className="bg-white"
+                    />
+                    <Label htmlFor="option-one" className="text-white">
+                      true
+                    </Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem
+                      value="x-false"
+                      id="option-two"
+                      className="bg-white"
+                    />
+                    <Label htmlFor="option-two" className="text-white">
+                      false
+                    </Label>
+                  </div>
+                </RadioGroup>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h2 className="text-white">Level</h2>
+                <Select>
+                  <SelectTrigger className="w-[180px] h-9 bg-black text-cyan-50 rounded-sm">
+                    <SelectValue placeholder="All" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-black text-cyan-50 border-none cursor-pointer">
+                    {selectLevel.map((item) => (
+                      <SelectItem
+                        key={item.value}
+                        className="bg-black text-cyan-50 border-none cursor-pointer"
+                        value={item.value}
+                      >
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h2 className="text-white">Type</h2>
+                <Select>
+                  <SelectTrigger className="w-[180px] h-9 bg-black text-cyan-50 rounded-sm">
+                    <SelectValue placeholder="All" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-black text-cyan-50 border-none cursor-pointer">
+                    {selectTypes.map((item) => (
+                      <SelectItem
+                        key={item.value}
+                        className="bg-black text-cyan-50 border-none cursor-pointer"
+                        value={item.value}
+                      >
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h2 className="text-white">Attribute</h2>
+                <Select>
+                  <SelectTrigger className="w-[180px] h-9 bg-black text-cyan-50 rounded-sm">
+                    <SelectValue placeholder="All" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-black text-cyan-50 border-none cursor-pointer">
+                    {selectAttribute.map((item) => (
+                      <SelectItem
+                        key={item.value}
+                        className="bg-black text-cyan-50 border-none cursor-pointer"
+                        value={item.value}
+                      >
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h2 className="text-white">Fields</h2>
+                <Select>
+                  <SelectTrigger className="w-[180px] h-9 bg-black text-cyan-50 rounded-sm">
+                    <SelectValue placeholder="All" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-black text-cyan-50 border-none cursor-pointer">
+                    {selectField.map((item) => (
+                      <SelectItem
+                        key={item.value}
+                        className="bg-black text-cyan-50 border-none cursor-pointer"
+                        value={item.value}
+                      >
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h2 className="text-white">Date of realease</h2>
+                <Select>
+                  <SelectTrigger className="w-[180px] h-9 bg-black text-cyan-50 rounded-sm">
+                    <SelectValue placeholder="All" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-black text-cyan-50 border-none cursor-pointer">
+                    {selectDate.map((item) => (
+                      <SelectItem
+                        key={item.value}
+                        className="bg-black text-cyan-50 border-none cursor-pointer"
+                        value={item.value}
+                      >
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <Button className="mt-4 p-2 bg-blue-500 text-white rounded">
+                Pesquisar
+              </Button>
+            </form>
+          </PopoverContent>
+        </Popover>
+      </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mb-[10px]">
         {pageDigimons.map((digimon) => (
           <li key={digimon.id} className="flex justify-center">
@@ -168,6 +278,6 @@ function Home() {
       )}
     </main>
   );
-}
+};
 
 export default Home;
