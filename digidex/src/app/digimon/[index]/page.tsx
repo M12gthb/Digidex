c2df6c0584b623a7cb13c411983dd422e63b99e7
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-07-08 16:31:16 -0300

    feat: catch the digimon for url and loading

--- digidex/src/app/digimon/[index]/page.tsx
+++ digidex/src/app/digimon/[index]/page.tsx
@@ -1,12 +1,52 @@
-import React from "react";
+"use client";
+import { IDigimonInfos } from "@/interfaces/Digimon";
+import { api } from "@/services/api";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
-function digimon({ params }: { params: { index: string } }) {
-  return (
-    <div className="min-h-[70vh]">
-      <h1>Digimon</h1>
-      <h2>{params.index}</h2>
-    </div>
-  );
+interface DigimonProps {
+  params: { index: string };
 }
 
-export default digimon;
+const Digimon: React.FC<DigimonProps> = ({ params }) => {
+  const [digimon, setDigimon] = useState<IDigimonInfos | undefined>(undefined);
+  useEffect(() => {
+    if (params.index === undefined) {
+      params.index === "1";
+    } else {
+      const getDigimon = async () => {
+        try {
+          const response = await api.get(`/digimon/${params.index}`);
+          setDigimon(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      getDigimon();
+    }
+  }, [params.index]);
+
+  if (!digimon) {
+    return (
+      <div className="min-h-[70vh] flex items-center justify-center flex-col">
+        <Image src={"/grey.gif"} width={300} height={300} alt="" />
+        <h1 className="text-white text-2xl">Procurando...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-[70vh]">
+      <h1>{digimon.name}</h1>
+      <Image
+        src={digimon.images[0].href}
+        alt={digimon.name}
+        width={200}
+        height={200}
+      />
+    </div>
+  );
+};
+
+export default Digimon;
