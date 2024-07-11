18fa74cae1f994c333455a75b4db102fab952a42
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-07-11 10:17:14 -0300

    style: CustomInput adjust

--- digidex/src/components/CustomInput/index.tsx
+++ digidex/src/components/CustomInput/index.tsx
@@ -9,15 +9,15 @@ interface CustomInputProps {
 
 const CustomInput: React.FC<CustomInputProps> = ({ onChange, value }) => {
   return (
-    <div className="border-none bg-zinc-700 rounded h-[40px] text-gray-300 relative">
+    <div className="border-none bg-zinc-700 rounded h-[40px] min-w-[205px] text-gray-300 relative">
       <Input
         type="search"
         placeholder="Search for your digimon here..."
-        className="border-none h-[100%] placeholder-custom text-xs sm:text-sm"
+        className="border-none h-[100%] placeholder-custom text-xs sm:text-xs"
         value={value}
         onChange={onChange}
       />
-      <IoSearchSharp className="absolute top-[8px] right-[10px] sm:top-[10px]" />
+      <IoSearchSharp className="absolute top-[12px] right-[7px] sm:top-[12px]" />
     </div>
   );
 };
